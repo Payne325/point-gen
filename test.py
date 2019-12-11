547d41a5ffa97deb9c7dd4ed52bec0c85b4640ce
Alex Payne <alex.payne@renishaw.com>
2019-12-11 09:38:18 +0000

    Adding test cases for sphere point generation

---
+++ test.py
@@ -0,0 +1,48 @@
+import unittest
+
+class TestSpherePointGenerationMethods(unittest.TestCase):
+
+    def test_can_generate_5_points_on_sphere_at_origin(self):
+        self.assertTrue(False)
+
+    def test_can_generate_10_points_on_sphere_at_origin(self):
+        self.assertTrue(False)
+
+    def test_can_generate_100_points_on_sphere_away_from_origin(self):
+        self.assertTrue(False)
+
+    def test_can_generate_5_points_on_sphere_away_from_origin(self):
+        self.assertTrue(False)
+
+    def test_can_generate_10_points_on_sphere_away_from_origin(self):
+        self.assertTrue(False)
+
+    def test_can_generate_100_points_on_sphere_away_from_origin(self):
+        self.assertTrue(False)
+
+    def test_can_generate_5_points_on_sphere_with_large_radius(self):
+        self.assertTrue(False)
+
+    def test_can_generate_10_points_on_sphere_with_large_radius(self):
+        self.assertTrue(False)
+
+    def test_can_generate_100_points_on_sphere_with_large_radius(self):
+        self.assertTrue(False)
+
+    def test_can_generate_5_points_on_sphere_with_small_radius(self):
+        self.assertTrue(False)
+
+    def test_can_generate_10_points_on_sphere_with_small_radius(self):
+        self.assertTrue(False)
+
+    def test_can_generate_100_points_on_sphere_with_small_radius(self):
+        self.assertTrue(False)
+
+    def test_exception_thrown_when_asked_to_generate_4_points_on_sphere(self):
+        self.assertTrue(False)
+
+    def test_exception_thrown_when_asked_to_generate_0_points_on_sphere(self):
+        self.assertTrue(False)
+
+if __name__=="__main__":
+    unittest.main()
